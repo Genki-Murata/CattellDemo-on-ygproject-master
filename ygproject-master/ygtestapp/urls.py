2ce41cbc384353fd8894a3bfbce68c39e4c1546a
github-kyuuhati <jojo1234dorararara@gmail.com>
2021-01-15 13:18:25 +0900

    ygテストのコントローラとルーティングからキャッテル を削除

--- ygproject-master/ygtestapp/urls.py
+++ ygproject-master/ygtestapp/urls.py
@@ -10,11 +10,4 @@ urlpatterns = [
     path('test_close/',views.test_close,name='test_close'),
     path('participant_index', views.participant_index, name='participant_index'),
     path('participant_show/<int:user_id>/', views.participant_show, name='participant_show'),
-
-    # キャッテル
-    path('cattell/detail/<int:page_no>/',views.cattell_detail,name='cattell_detail'),
-    path('cattell/example/<int:page_no>/',views.cattell_example,name='cattell_example'),
-    path('cattell/test/<int:page_no>/',views.cattell_test,name='cattell_test'),
-    path('cattell/break',views.cattell_break,name='cattell_break'),
-    #
 ]
