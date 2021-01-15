b8b7b5b47bca538bb7bd9cd7892c36e1546cd9a3
github-kyuuhati <jojo1234dorararara@gmail.com>
2021-01-15 13:14:47 +0900

    別アプリへのルーティング_検証

--- ygproject-master/ygproject/urls.py
+++ ygproject-master/ygproject/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ygtestapp.urls')),
+    path('cattell/', include('cattellApp.urls')),    
 ]
